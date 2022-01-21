25a025820d39df03d139501804e3ad6ddf53f744
sjasonhumphrey <sjasonhumphrey@gmail.com>
2022-01-21 14:17:15 -0600

    added pictures to projects/setup e-mail form

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "The Low End",
     description:
       "A full-stack application that uses React, Redux, GraphQL, Sass and Firebase. It also uses the Stripe API for online purchasing. It is a fictional site for a high-end music retailer.",
-    image: "./project-1.gif",
+    image: "./The_Low_End.png",
     link: "https://the-low-end.netlify.app/",
   },
   {
@@ -12,7 +12,7 @@ export const projects = [
     subtitle: "Coding Buddy",
     description:
       "It was designed to be a resource for new developers. It has a typing tutor, technical quiz, flashcards, codepen and developer resource links. It uses one API for the technical trivia questions.",
-    image: "./project-2.gif",
+    image: "./Coding_Buddy.png",
     link: "http://www.coding-buddy.club/",
   },
   {
@@ -20,7 +20,7 @@ export const projects = [
     subtitle: "Monster Match",
     description:
       "A small app built using React and CSS. It is based on the classic card game Memory. The cards feature the familiar faces of the best known Universal Monsters. Great to play with your friends and family.",
-    image: "./project-3.gif",
+    image: "./Monster_Match.png",
     link: "https://dreamy-curran-6e407a.netlify.app/",
   },
   {
@@ -28,7 +28,7 @@ export const projects = [
     subtitle: "Vanilla Javascript",
     description:
       " An app that provides the user with information on movies. It will show if a movie is streaming and which service it can currently be seen on. The OMDB API was used.",
-    image: "./project-4.gif",
+    image: "./Streamo.png",
     link: "https://findingstream.netlify.app/",
   },
 ];
@@ -36,10 +36,10 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
+      `"Every Project Jason Presents Leaves Me Absolutely Floored! I Aspire To Be As Talented As He Is With My Coding One Day."`,
+    image: "./Andreana.jpeg",
+    name: "Andrena Alexandre",
+    company: "DigitalCrafts",
   },
   {
     quote:
