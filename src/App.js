08f5c1d6ebe9e3f09f40ea3d38f583f7fb1cb945
SJasonHumphrey <sjasonhumphrey@gmail.com>
2023-09-24 00:05:29 -0500

    removed fade

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
-import Fade from "react-reveal/Fade";
 
 export default function App() {
   return (
     <main>
       <Navbar className="text-gray-400 bg-gray-900 body-font"/>
-      <Fade top>
       <About />
-      </Fade>
       <Projects />
       <Skills />
       <Testimonials />
